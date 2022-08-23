4e915f257f09e028edf304b2ee38c3e56dcb35b2
sendra <polsendra@gmail.com>
2022-08-23 16:54:22 +0200

    fix: remove unneeded log

--- script/DeployENSPayload.s.sol
+++ script/DeployENSPayload.s.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import 'forge-std/console.sol';
 import 'forge-std/Script.sol';
 import {ENSListingPayload} from 'src/contracts/ENSListingPayload.sol';
 
@@ -10,7 +9,6 @@ contract DeployENSListingPayload is Script {
         vm.startBroadcast();
 
         address ensListingPayload = address(new ENSListingPayload());
-        console.log('ENS Listing Payload address', ensListingPayload);
 
         vm.stopBroadcast();
     }
